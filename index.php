eaa223c38cacafabc49fdfb929d80e855f3d79a8
Farzan Ali <farzanali1296@gmail.com>
2017-01-28 22:50:47 -0500

    added item list and input form

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
+    <div class="list">
+      <h1 class="header">To do.</h1>
 
+      <ul>
+        <li><span class="item">Pick up books</span></li>
+        <li><span class="item">Learn PHP</span></li>
+      </ul>
+
+      <form class="item-add" action="add.php" method="post">
+        <input type="text" name="name" placeholder="Type a new item here" class="input" autocomplete="off" required>
+        <input type="submit" value="Add" class="submit">
+      </form>
+    </div>
   </body>
 </html>
