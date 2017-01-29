9278e1e9da64168b34a4125b81a2f669cde51156
Farzan Ali <farzanali1296@gmail.com>
2017-01-28 22:44:42 -0500

    set up HTML elements

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>To do</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+
+  </body>
+</html>
