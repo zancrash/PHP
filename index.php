1742380f32331f15fc5ae9ec41d34b2001a13488
Farzan Ali <farzanali1296@gmail.com>
2017-01-28 23:01:15 -0500

    Changed a font source

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="utf-8">
     <title>To do</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light+Two" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   </head>
   <body>
     <div class="list">
-      <h1 class="header">To do.</h1>
+      <h1 class="header">To Do.</h1>
 
       <ul>
         <li><span class="item">Pick up books</span></li>
